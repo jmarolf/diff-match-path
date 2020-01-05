efe6c3856759f565c2962add9517aefa313b8ba8
Jonathon Marolf <jmarolf@microsoft.com>
2020-01-04 23:48:45 -0800

    remove usings

--- src/Microsoft.CodeAnalysis.Diff/Patcher.cs
+++ src/Microsoft.CodeAnalysis.Diff/Patcher.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Text;
 
 namespace Microsoft.CodeAnalysis.DiffMatchPatch {
     public static class Patcher {
