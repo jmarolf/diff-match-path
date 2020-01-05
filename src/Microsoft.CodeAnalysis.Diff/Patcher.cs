b4f70b3cb563292a073af152570c57660f6cb94d
Jonathon Marolf <jmarolf@microsoft.com>
2020-01-04 23:46:08 -0800

    adding new api stubs

---
+++ src/Microsoft.CodeAnalysis.Diff/Patcher.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Text;
+
+namespace Microsoft.CodeAnalysis.DiffMatchPatch {
+    public static class Patcher {
+        public static ImmutableArray<Patch> ComputePatch(string left, string right) {
+            return ComputePatch(left.AsSpan(), right.AsSpan());
+        }
+
+        public static ImmutableArray<Patch> ComputePatch(ReadOnlySpan<char> left, ReadOnlySpan<char> right) {
+            throw new NotImplementedException();
+        }
+
+        public static ImmutableArray<Patch> ComputePatch(ImmutableArray<Diff> diffs) {
+            throw new NotImplementedException();
+        }
+
+        public static ImmutableArray<Patch> ComputePatch(ReadOnlySpan<char> text, ImmutableArray<Diff> diffs) {
+            throw new NotImplementedException();
+        }
+
+        public static ImmutableArray<Patch> ComputePatch(ReadOnlySpan<char> left, ReadOnlySpan<char> right, ImmutableArray<Diff> diffs) {
+            throw new NotImplementedException();
+        }
+
+        public static PatchResult ApplyPatch(ImmutableArray<Patch> patches, string text) {
+            throw new NotImplementedException();
+        }
+
+        public static (bool Success, PatchResult PatchResult) TryApplyPatch(ImmutableArray<Patch> patches, string text) {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class PatchResult {
+    }
+
+    public class Patch {
+    }
+}
